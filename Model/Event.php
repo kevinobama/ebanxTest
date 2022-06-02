22681409089157b6efa542d7f5e00fd607829989
kevin Gates <kevinobama@gmail.com>
2022-06-02 17:52:40 +0800

    update

--- Model/Event.php
+++ Model/Event.php
@@ -1,11 +1,18 @@
 <?php
-
 class Event {
+    public static $balance=0;
 
     public function  createEvent($data) {
         if($data['type']=="deposit") {
+            $oldbalance =0;
+            if(file_exists("balance.txt"))
+                $oldbalance=file_get_contents('balance.txt');
 
-            return array("data"=>array('destination'=> array('id'=> $data['destination'],'balance'=>$data['amount'])),'code'=>'201');
+            $newBalance = $oldbalance+$data['amount'];
+
+            file_put_contents('balance.txt', $data['amount']);
+
+            return array("data"=>array('destination'=> array('id'=> $data['destination'],'balance'=>$newBalance)),'code'=>'201');
         } elseif ($data['type']=="withdraw") {
             if($data['origin']=="100")//exist
                 return array("data"=>array('origin' => array('id'=> $data['origin'],'balance'=>'15')),'code'=>'201');
