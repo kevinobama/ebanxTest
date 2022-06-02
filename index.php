d05850781ee6dd2e185d1546eec1aea233f5ab26
kevin Gates <kevinobama@gmail.com>
2022-06-02 15:07:32 +0800

    update

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
 $request = $_SERVER['REQUEST_URI'];
+//GET /balance
 
 switch ($request) {
     case '/' :
         echo '/views/index.php';
         break;
-    case '' :
-        echo '/views/index.php';
+    case 'balance' :
+        echo "0";
         break;
     case '/about' :
         echo '/views/about.php';
+        break;
+    case '/balance' :
+        echo '/views/about.php';
+        break;
+    case '/event' :
+        echo '{"destination": {"id":"100", "balance":10}}';
         break;
     default:
         echo '/views/404.php';
