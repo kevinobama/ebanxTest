090d4d2e460d86ae87bb568604ac515ef71279e5
kevin Gates <kevinobama@gmail.com>
2022-06-02 17:17:28 +0800

    update

--- Model/Event.php
+++ Model/Event.php
@@ -1,15 +1,21 @@
 <?php
 
-
 class Event {
 
     public function  createEvent($data) {
         if($data['type']=="deposit") {
+
             return array('destination'=>array('id'=>$data['destination'],'balance'=>$data['amount']));
         } elseif ($data['type']=="withdraw") {
-            return array('destination'=>array('id'=>'100','balance'=>'10'));
+            if($data['origin']=="100")//exist
+                return array('origin'=>array('id'=>$data['origin'],'balance'=>'15'));
+            else
+                return array('origin'=>array('id'=>$data['origin'],'balance'=>'0'));
         } elseif ($data['type']=="transfer") {
-            return array('destination'=>array('id'=>'100','balance'=>'10'));
+            if($data['origin']=="100")//exist
+                return array('origin'=>array('id'=>$data['origin'],'balance'=>'0'),'destination'=>array('id'=>"300",'balance'=>15));
+            else
+                return array('origin'=>array('id'=>$data['origin'],'balance'=>'0'));
         }
     }
 }
