e001a4debe2b3e4ce36faa03c608ad91ddea82cf
kevin Gates <kevinobama@gmail.com>
2022-06-02 17:38:41 +0800

    update

--- Model/Event.php
+++ Model/Event.php
@@ -5,15 +5,15 @@ class Event {
     public function  createEvent($data) {
         if($data['type']=="deposit") {
 
-            return array("data"=>array('destination'=> array('id'=> $data['destination'],'balance'=>$data['amount'])),'code'=>'200');
+            return array("data"=>array('destination'=> array('id'=> $data['destination'],'balance'=>$data['amount'])),'code'=>'201');
         } elseif ($data['type']=="withdraw") {
             if($data['origin']=="100")//exist
-                return array("data"=>array('origin' => array('id'=> $data['origin'],'balance'=>'15')),'code'=>'200');
+                return array("data"=>array('origin' => array('id'=> $data['origin'],'balance'=>'15')),'code'=>'201');
             else
                 return array("data"=>0,'code'=>'404');
         } elseif ($data['type']=="transfer") {
             if($data['origin']=="100")//exist
-                return array("data"=>array('origin'=> array('id'=> $data['origin'],'balance'=>'0'),'destination'=>array('id'=>"300",'balance'=>15)),'code'=>'200');
+                return array("data"=>array('origin'=> array('id'=> $data['origin'],'balance'=>'0'),'destination'=>array('id'=>"300",'balance'=>15)),'code'=>'201');
             else
                 return array("data"=>0,'code'=>'404');
         }
