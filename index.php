1e33b89ca60426e7b8438d5a7f2f3eb93e15216b
kevin Gates <kevinobama@gmail.com>
2022-06-02 15:18:28 +0800

    update

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 $request = $_SERVER['REQUEST_URI'];
 //GET /balance
-
+echo($request.PHP_EOL);
 switch ($request) {
     case '/' :
         echo '/views/index.php';
         break;
-    case 'reset' :
+    case '/reset' :
         echo "OK";
         break;
     case '/about' :
@@ -14,6 +14,13 @@ switch ($request) {
         break;
     case '/balance' :
         echo '0';
+        break;
+    case '/balance' :
+        echo '0';
+        break;
+    case '/balance?account_id=1234':
+        http_response_code(404);
+        echo json_encode(array('0'));
         break;
     case '/event' :
         echo '{"destination": {"id":"100", "balance":10}}';
